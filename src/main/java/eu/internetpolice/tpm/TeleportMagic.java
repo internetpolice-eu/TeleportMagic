c39b43e5011c8fa4fc70bd2a367fb7a4967100a2
Muspah <simondw@sapo.pt>
2020-07-13 14:26:13 +0100

    Initial commit - basic plugin framework

---
+++ src/main/java/eu/internetpolice/tpm/TeleportMagic.java
@@ -0,0 +1,15 @@
+package eu.internetpolice.tpm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TeleportMagic extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
